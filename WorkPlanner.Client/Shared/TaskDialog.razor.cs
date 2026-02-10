50b73240d31df429ff187539b0f71f7b34afa605
Patryk Rakowski <patrykr2001@gmail.com>
2026-02-10 22:11:02 +0100

    refactor: inline task dialog logic

--- WorkPlanner.Client/Shared/TaskDialog.razor.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Components;
-using MudBlazor;
-
-namespace WorkPlanner.Client.Shared;
-
-public partial class TaskDialog
-{
-    [CascadingParameter] private IMudDialogInstance MudDialog { get; set; } = null!;
-}
